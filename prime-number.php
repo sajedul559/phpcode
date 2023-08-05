0a329111daae6ea7efc2089fad4c44512c9a55bb
sajedul559 <sajedulkhairul@gmail.com>
2023-08-05 22:56:30 +0600

    add recursive function

---
+++ prime-number.php
@@ -0,0 +1,21 @@
+<?php  
+$count = 0;  
+$num = 2;  
+while ($count < 20 )  
+{  
+    $div_count=0;  
+    for ( $i=1; $i<=$num; $i++)  
+    {  
+        if (($num%$i)==0)  
+        {  
+        $div_count++;  
+        }  
+    }  
+    if ($div_count<3)  
+    {  
+        echo $num." , ";  
+        $count=$count+1;  
+    }  
+    $num=$num+1;  
+}  
+?>  
